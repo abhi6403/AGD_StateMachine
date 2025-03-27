79b954b3affc6fbdf563d7060f5f216286ce7b5b
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2025-03-27 09:24:48 +0530

    created generic state machine

--- Assets/Scripts/StateMachine/IStateMachine.cs
+++ Assets/Scripts/StateMachine/IStateMachine.cs
@@ -1,12 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using StatePattern.Enemy;
 using UnityEngine;
 
 namespace StatePattern.StateMachine
 {
-    public interface IStateMachine
+    public class GenericStateMachine<T> where T : EnemyController
     {
-        public void ChangeState(States newState);
+        protected T Owner;
+        protected IState currentState;
+        
+        protected Dictionary<States, IState> States = new Dictionary<States, IState>();
+        public GenericStateMachine(T owner) => this.Owner = owner;
+        
+        public void Update() => currentState?.Update();
+
+        protected void ChangeState(IState newState)
+        {
+            currentState?.OnStateExit();
+            currentState = newState;
+            currentState?.OnStateEnter();
+        }
+
+        protected void SetOwner()
+        {
+            foreach (IState state in States.Values)
+            {
+                state.Owner = Owner;
+            }
+        }
+        public void ChangeState(States newState) => ChangeState(States[newState]);
     }
 
     public enum States
