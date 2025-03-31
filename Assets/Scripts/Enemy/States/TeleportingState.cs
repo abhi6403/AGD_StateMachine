4b12ff5605b119adb1ec4974e7a4ffbe6e750c96
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2025-03-31 12:10:21 +0530

    Created Hitman and level 3

--- Assets/Scripts/Enemy/States/TeleportingState.cs
+++ Assets/Scripts/Enemy/States/TeleportingState.cs
@@ -1,6 +1,36 @@
+using StatePattern.StateMachine;
 using UnityEngine;
 
-public class TeleportingState<T> 
+namespace StatePattern.Enemy
 {
+    public class TeleportingState<T> : IState where T : EnemyController
+    {
+        public EnemyController Owner { get; set; }
+        private GenericStateMachine<T> stateMachine;
+        
+        public TeleportingState(GenericStateMachine<T> stateMachine) => this.stateMachine = stateMachine;
+
+        public void OnStateEnter()
+        {
+            TeleportToRandomPosition();
+            stateMachine.ChangeState(States.CHASING);
+        }
+
+        public void Update() { }
+
+        public void OnStateExit() { }
+        
+        private void TeleportToRandomPosition() => Owner.Agent.Warp(GetRandomNavMeshPoint());
+        
+        private Vector3 GetRandomNavMeshPoint()
+        {
+            Vector3 randomDirection = Random.insideUnitSphere * 5f + Owner.Position;
+            UnityEngine.AI.NavMeshHit hit;
+            
+            if (UnityEngine.AI.NavMesh.SamplePosition(randomDirection, out hit, 5f, UnityEngine.AI.NavMesh.AllAreas))
+                return hit.position;
     
+            return Owner.Data.SpawnPosition;
+        }
+    }
 }
